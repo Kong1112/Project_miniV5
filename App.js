9a1f0a3c95d6b7c5d8aafb59aef9eb40dab29fc8
Kong1112 <kong110943@gmail.com>
2023-02-04 18:16:47 +0700

    Mini_Project

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider } from "native-base";
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+import StackNav from "./Navigations/StackNav";
+import CartScreen from "./screens/CartScreen";
+import Search from "./componenst/Search";
+import SearchTest from "./screens/SearchTest";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+ export default function App() {
+   return (
+     <NativeBaseProvider>
+       <NavigationContainer>
+         <StackNav/>
+       </NavigationContainer>
+     </NativeBaseProvider>
+   );
+ }
